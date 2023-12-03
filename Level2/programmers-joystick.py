9bec0b6ca15beecf6d1de7377896eb616a39b19f
barom_kim <ynrnwa@gmail.com>
2023-12-03 20:29:45 +0900

    add annotation

--- Level2/programmers-joystick.py
+++ Level2/programmers-joystick.py
@@ -1,20 +1,33 @@
 # 프로그래머스 조이스틱
 def solution(name):
     answer = 0
+    # 좌우로 이동하는 최소 횟수는 전체길이 - 1
     move_cnt = len(name) - 1
-    
+
     for i, spell in enumerate(name):
+        # 상 방향키로 가는 횟수와 하 방향키로 가는 횟수 중 작은 값으로 더 해줌
         answer += min(ord(spell) - ord('A'), ord('Z') - ord(spell) + 1)
-        
+        # 다음 글자로 이동하는 인덱스
         next = i + 1
-        
+
+        # 이동한 위치가 전체 길이보다 작아야 하고,
+        # 이동한 위치의 글자가 A라면
+        # 계속 이동
         while next < len(name) and name[next] == 'A':
             next += 1
-        
+        # 좌우 방향키로 이동하는 횟수를 더함(아래 3가지 중 작은값으로)
+        # 1.이전까지 계산된 최소 이동 횟수
+        # 2.현재 위치에서 시작점(idx=0)으로 돌아간 다음, 남은 문자들을 처리하기 위해 다시 끝까지 이동하는 경우
+        # '2*1' : 현재 위치에서 시작점으로 돌아가는데 드는 이동 횟수(왕복이기 때문에 2배)
+        # 'len(name) - next' : 남은 문자들을 처리하기 위해 다시 끝까지 이동하는 횟수
+        # 3.시작점에서 현재 위치까지 이동한 다음, 남은 문자들을 처리하기 위해 끝까지 갔다가 다시 돌아오는 경우
+        # 'len(name) - next : 남은 문자들에 대한 이동 횟수이고, *2를 하는 이유는 다시 돌아와야 하기 때문에
         move_cnt = min([move_cnt, 2 * i + len(name) - next, i + 2 * (len(name) - next)])
-    
+
+    # 총 조작 횟수에서 좌우 이동하는 횟수를 더함
     answer += move_cnt
-    
+
     return answer
 
 print(solution('JEROEN'))
+print(solution('JAN'))
--- Level2/programmers-split_two_divide_powerline.py
+++ Level2/programmers-split_two_divide_powerline.py
@@ -5,8 +5,10 @@ def bfs(start, graph, visited, check_link):
     dq = deque([start])
     visited[start] = True
     cnt = 1
+
     while dq:
         s = dq.popleft()
+
         for i in graph[s]:
             if visited[i] == False and check_link[start][i]:
                 dq.append(i)
@@ -17,18 +19,26 @@ def bfs(start, graph, visited, check_link):
 def solution(n, wires):
     answer = n
 
+    # 예시1 : 10 X 10
     check_link = [[True] * (n+1) for _ in range(n+1)]
+    # 예시1 : [[], [], [], [], [], [], [], [], [], []]
     graph = [[] for _ in range(n+1)]
 
+    # 예시1 : [[], [3], [3], [1, 2, 4], [3, 5, 6, 7], [4], [4], [4, 8, 9], [7], [7]]
     for i, j in wires:
         graph[i].append(j)
         graph[j].append(i)
 
     for x, y in wires:
+        # 예시1 = [[False], [False], [False], [False], [False], [False], [False], [False], [False], [False]]
         visited = [False] * (n+1)
+        # x -> y 선 차단
         check_link[x][y] = False
+        # x 기준으로 연결된 송전탑 개수
         cnt_x = bfs(x, graph, visited, check_link)
+        # y 기준으로 연결된 송전탑 개수
         cnt_y = bfs(y, graph, visited, check_link)
+        # 차단한 선 초기화
         check_link[x][y] = True
 
         answer = min(answer, abs(cnt_x - cnt_y))
