f97194ae6f9caa0678bf72d83c48c705ea5503fb
RomkDB <ynrnwa@gmail.com>
2024-05-10 22:03:51 +0900

    add other solve

--- Bronze2/boj_2920.py
+++ Bronze2/boj_2920.py
@@ -4,8 +4,10 @@ asc = True
 desc = True
 
 for i in range(1, 8):
+    # 앞의 수보다 큰 경우 -> 내림차가 아님
     if line[i] > line[i - 1]:
         desc = False
+    # 앞의 수보다 작은 경우 -> 오름차가 아님
     elif line[i] < line[i - 1]:
         asc = False
 
@@ -15,3 +17,13 @@ elif desc:
     print('descending')
 else:
     print('mixed')
+
+# 다른 풀이
+# line = list(map(int, input().split()))
+
+# if line == sorted(line):
+#     print('asceding')
+# elif line == sorted(line, reverse=True):
+#     print('descending')
+# else:
+#     print('mixed')
